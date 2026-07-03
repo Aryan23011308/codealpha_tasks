06c6b11f948d410a07dd900d7098ec0e3e707f40
Aryan23011308 <aryantomar75831@gmail.com>
2026-07-03 20:36:24 +0530

    Add files via upload

---
+++ Task4_Objectdetection/app.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+
+    if not success:
+        break
+
+    results = model.track(frame, persist=True, conf=0.5)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("Object Detection and Tracking", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
